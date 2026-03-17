01121e709c5f956d4253ee29243b38cefb732e2e
Mikey McCormack <michaeljohnmccormack@gmail.com>
2026-03-17 19:43:12 -0400

    Add more tests for logout using the new BDD style

--- backend/app/tests/test_auth_api.py
+++ backend/app/tests/test_auth_api.py
@@ -15,39 +15,37 @@ def db():
     return deps.db
 
 
-def describe_auth():
-    def describe_login():
-        def describe_when_no_credentials():
-            def it_returns_400(client):
-                response = client.post("/api/auth/login", json={})
-                assert response.status_code == 400
-                assert response.json == {"error": "No data provided"}
+def describe_login():
+    def describe_when_no_credentials():
+        def it_returns_400(client):
+            response = client.post("/api/auth/login", json={})
+            assert response.status_code == 400
+            assert response.json == {"error": "No data provided"}
 
-        def describe_with_email_and_no_password():
-            def it_returns_400(client):
-                response = client.post(
-                    "/api/auth/login", json={"email": "mc@hammer.com"}
-                )
-                assert response.status_code == 400
-                assert response.json == {"error": "Email and password are required"}
+    def describe_with_email_and_no_password():
+        def it_returns_400(client):
+            response = client.post("/api/auth/login", json={"email": "mc@hammer.com"})
+            assert response.status_code == 400
+            assert response.json == {"error": "Email and password are required"}
 
-        def describe_with_password_and_no_email():
-            def it_returns_400(client):
-                response = client.post("/api/auth/login", json={"password": "bananas"})
-                assert response.status_code == 400
-                assert response.json == {"error": "Email and password are required"}
+    def describe_with_password_and_no_email():
+        def it_returns_400(client):
+            response = client.post("/api/auth/login", json={"password": "bananas"})
+            assert response.status_code == 400
+            assert response.json == {"error": "Email and password are required"}
 
-        def describe_with_user_not_found():
-            def it_returns_401(client):
-                db.get_user_by_email = Mock(return_value=None)
+    def describe_with_user_not_found():
+        def it_returns_401(client):
+            db.get_user_by_email = Mock(return_value=None)
 
-                response = client.post(
-                    "/api/auth/login",
-                    json={"email": "mc@hammer.com", "password": "bananas"},
-                )
-                assert response.status_code == 401
-                assert response.json == {"error": "Invalid email or password"}
+            response = client.post(
+                "/api/auth/login",
+                json={"email": "mc@hammer.com", "password": "bananas"},
+            )
+            assert response.status_code == 401
+            assert response.json == {"error": "Invalid email or password"}
 
+    def describe_user_password_checking():
         def describe_user_without_password_hash():
             def it_returns_401(client, db):
                 db.get_user_by_email = Mock(
@@ -60,6 +58,28 @@ def describe_auth():
                 )
                 assert response.status_code == 401
                 assert response.json == {"error": "Please sign in with Google"}
+
+        def describe_how_the_password_check_works():
+            @patch("app.auth.check_password_hash")
+            def it_calls_werkzeug_check_password_hash(mock_check_password, client, db):
+                db.get_user_by_email = Mock(
+                    return_value={
+                        "email": "mc@hammer.com",
+                        "password_hash": "stored-password-hash",
+                        "password": "stored-password",
+                    }
+                )
+
+                mock_check_password.return_value = False
+
+                client.post(
+                    "/api/auth/login",
+                    json={"email": "mc@hammer.com", "password": "provided-password"},
+                )
+
+                mock_check_password.assert_called_with(
+                    "stored-password-hash", "provided-password"
+                )
 
         def describe_with_user_password_hash_mismatch():
             @patch("app.auth.check_password_hash")
@@ -81,73 +101,93 @@ def describe_auth():
                 assert response.status_code == 401
                 assert response.json == {"error": "Invalid email or password"}
 
-        def describe_with_user_not_verified():
-            @patch("app.auth.check_password_hash")
-            def it_returns_403(mock_check_password, client, db):
-                db.get_user_by_email = Mock(
-                    return_value={
-                        "email": "mc@hammer.com",
-                        "password_hash": "fake-hash",
-                        "is_verified": False,
-                    }
-                )
-
-                mock_check_password.return_value = True
-
-                response = client.post(
-                    "/api/auth/login",
-                    json={"email": "mc@hammer.com", "password": "bananas"},
-                )
-                assert response.status_code == 403
-                assert response.json == {
-                    "error": "Email not verified. Please check your inbox."
-                }
-
-        def describe_given_successful():
-            @patch("app.auth.check_password_hash")
-            def it_returns_200_and_user_information(mock_check_password, client, db):
-                db.get_user_by_email = Mock(
-                    return_value={
-                        "email": "mc@hammer.com",
-                        "password_hash": "fake-hash",
-                        "is_verified": True,
-                        "id": 1234,
-                        "name": "MC Hammer",
-                        "picture": "some-picture",
-                    }
-                )
-                mock_check_password.return_value = True
-                db.update_last_login = Mock()
-
-                response = client.post(
-                    "/api/auth/login",
-                    json={"email": "mc@hammer.com", "password": "bananas"},
-                )
-                assert response.status_code == 200
-                assert response.json["message"] == "Login successful"
-                assert response.json["user"] == {
-                    "id": 1234,
+    def describe_with_user_not_verified():
+        @patch("app.auth.check_password_hash")
+        def it_returns_403(mock_check_password, client, db):
+            db.get_user_by_email = Mock(
+                return_value={
                     "email": "mc@hammer.com",
+                    "password_hash": "fake-hash",
+                    "is_verified": False,
+                }
+            )
+
+            mock_check_password.return_value = True
+
+            response = client.post(
+                "/api/auth/login",
+                json={"email": "mc@hammer.com", "password": "bananas"},
+            )
+            assert response.status_code == 403
+            assert response.json == {
+                "error": "Email not verified. Please check your inbox."
+            }
+
+    def describe_given_successful():
+        @patch("app.auth.check_password_hash")
+        def it_returns_200_and_user_information(mock_check_password, client, db):
+            db.get_user_by_email = Mock(
+                return_value={
+                    "email": "mc@hammer.com",
+                    "password_hash": "fake-hash",
+                    "is_verified": True,
+                    "id": 1234,
                     "name": "MC Hammer",
                     "picture": "some-picture",
-                    "has_completed_onboarding": False,
                 }
+            )
+            mock_check_password.return_value = True
+            db.update_last_login = Mock()
 
-        def describe_given_error():
-            @patch("app.auth.check_password_hash")
-            def it_returns_500_and_error_message(mock_check_password, client, db):
-                db.get_user_by_email = Mock(
-                    return_value={
-                        "email": "mc@hammer.com",
-                        "password_hash": "fake-hash",
-                        "is_verified": True,
-                    }
-                )
-                mock_check_password.return_value = True
+            response = client.post(
+                "/api/auth/login",
+                json={"email": "mc@hammer.com", "password": "bananas"},
+            )
+            assert response.status_code == 200
+            assert response.json["message"] == "Login successful"
+            assert response.json["user"] == {
+                "id": 1234,
+                "email": "mc@hammer.com",
+                "name": "MC Hammer",
+                "picture": "some-picture",
+                "has_completed_onboarding": False,
+            }
 
-                response = client.post(
-                    "/api/auth/login",
-                    json={"email": "mc@hammer.com", "password": "bananas"},
-                )
-                assert response.status_code == 500
-                assert response.json == {"error": "'id'"}
+    def describe_given_error():
+        @patch("app.auth.check_password_hash")
+        def it_returns_500_and_error_message(mock_check_password, client, db):
+            db.get_user_by_email = Mock(
+                return_value={
+                    "email": "mc@hammer.com",
+                    "password_hash": "fake-hash",
+                    "is_verified": True,
+                }
+            )
+            mock_check_password.return_value = True
+
+            response = client.post(
+                "/api/auth/login",
+                json={"email": "mc@hammer.com", "password": "bananas"},
+            )
+            assert response.status_code == 500
+            assert response.json == {"error": "'id'"}
+
+        # @patch("app.auth.session")
+
+
+def describe_logout():
+    def it_returns_200_and_a_message(client):
+        response = client.post("/api/auth/logout")
+
+        # assert mock_session.clear.called
+        assert response.status_code == 200
+        assert response.json == {"message": "Logged out successfully"}
+
+    def it_clears_the_session(client):
+        with client.session_transaction() as sess:
+            sess["user_id"] = {"id": 1234}
+
+        client.post("/api/auth/logout")
+
+        with client.session_transaction() as sess:
+            assert "user_id" not in sess
